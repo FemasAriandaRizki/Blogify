fa808f9b7a6480ea7b33832d8b37aee7fad1e99d
femas arianda <femasariandarizki@students.undip.ac.id>
2024-11-25 23:56:35 +0700

    first commit

---
+++ resources/views/components/halaman-layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ isset($title)?$title: 'Judul Default' }}</title>
+</head>
+<body>
+    {{ $slot }}
+    <h1>Halo kontol</h1>
+</body>
+</html>
---
+++ resources/views/welcome3.blade.php
@@ -0,0 +1,7 @@
+<?php
+$judul = "Ini adalah judul dari class component"
+?>
+
+<x-halaman-layout :title="$judul">
+    <x-slot name="tanggal">25 November 2024</x-slot>
+</x-halaman-layout>
