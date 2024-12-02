4f0dbbb7629d2d6161d3a346ecc74f7b2307a4cb
Naufal Yoga Pratama <naufaltino2113@gmail.com>
2024-12-03 02:16:53 +0700

    syling

--- resources/views/components/front/layout.blade.php
+++ resources/views/components/front/layout.blade.php
@@ -24,7 +24,7 @@
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="site-heading">
-                            <a href='{{ $pageHeaderLink }}'>
+                            <a href='{{ $pageHeaderLink }}' class="text-white">
                                 <h1>{{ $pageHeader }}</h1>
                             </a>
                             @isset($padeSubheading)
